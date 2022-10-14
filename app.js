c675eb14413078a81e7f41db295eb8f2edf1be23
Tuugii <tuugii.1980.b@gmail.com>
2022-10-14 22:01:24 +0800

    hop3 node express demo

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+var users = [];
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.get("/greet/:name", (req, res) => {
+  res.send("Hello, " + req.params.name);
+});
+
+app.post("/user", (req, res) => {
+  users.push(req.body);
+  console.log(users);
+  res.send("user created");
+});
+
+app.patch("/user", (req, res) => {
+  users.map((el) => {
+    if (el.email === req.body.email) {
+      (el.name = "TugsTuguldur"), (el.password = "12345");
+    }
+    console.log(users);
+    res.send("user not found");
+  });
+});
+
+app.listen(3000, () => {
+  console.log("servers is running");
+});
+
+
+//how to run X
+//nodemon app.js
