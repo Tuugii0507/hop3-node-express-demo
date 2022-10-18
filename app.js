884a46e1bd632f82fe08cb1abe381b88f4e18ebc
Tuugii <tuugii.1980.b@gmail.com>
2022-10-18 22:14:25 +0800

    hop3 node-express task03

--- app.js
+++ app.js
@@ -2,37 +2,46 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+let words = {
+  classroom: "анги",
+  guy: "залуу",
+  ladie: "бүсгүй",
+  book: "ном",
+  food: "хоол",
+  phone: "утас",
+  dialogue: "харилцан яриа",
+};
 
-var users = [];
-
-app.get("/", (req, res) => {
-  res.send("hello");
+app.get("/:name", (req, res) => {
+  if (words[req.params.name]) {
+    res.send("translation : " + words[req.params.name]);
+  } else {
+    res.send("word not found");
+  }
 });
 
-app.get("/greet/:name", (req, res) => {
-  res.send("Hello, " + req.params.name);
+app.post("/add_word", (req, res) => {
+  words = { ...words, ...req.body };
+  console.log(words);
+  res.send("Added a word");
 });
 
-app.post("/user", (req, res) => {
-  users.push(req.body);
-  console.log(users);
-  res.send("user created");
-});
-
-app.patch("/user", (req, res) => {
-  users.map((el) => {
-    if (el.email === req.body.email) {
-      (el.name = "TugsTuguldur"), (el.password = "12345");
+app.put("/edit_word", (req, res) => {
+  for (let word in words) {
+    for (let newWord in req.body) {
+      if (word === newWord) {
+        words[word] = req.body[newWord];
+      }
     }
-    console.log(users);
-    res.send("user not found");
-  });
+  }
+  res.send("Words edited");
+});
+
+app.delete("/delete_word", (req, res) => {
+  delete words[Object.keys(req.body)]
+  res.send( words);
 });
 
 app.listen(3000, () => {
   console.log("servers is running");
 });
-
-
-//how to run hehe
-//nodemon app.js
